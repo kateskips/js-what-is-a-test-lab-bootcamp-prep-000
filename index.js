2ac74265fee2d36e2e14b2e9ef55e67d96474ab3
kateskips <follett.katie@gmail.com>
2018-07-25 16:09:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 var name = "Susan";
 var height = 30;
-var message = ` is ${height} inches tall`;
+var message = ` is ${height} inches tall + ${name}`;
 
 
 module.exports = {name, height, message};
